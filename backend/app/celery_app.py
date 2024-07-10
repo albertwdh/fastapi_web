9d484808a96624f126b2db64c30846dbeca8bdc3
albertwdh <albertwangdh@sina.com>
2024-07-10 14:36:23 +0800

    app

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -17,7 +17,7 @@ celery_app = Celery(
     "worker",
     broker="redis://localhost:6379/0",
     backend="redis://localhost:6379/0",
-    include=['app.task.test']
+    include=['task.test']
 )
 
 
